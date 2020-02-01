5790e420ad6f5d5914b6dd2340cb2a0740c4a8aa
vickitingle <rose@theideashouse.com>
2020-02-01 12:22:22 +0000

    Prototype for title picker. Two canvases: title picker and main.

---
+++ Assets/Scripts/LowerThirdHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LowerThirdHandler : MonoBehaviour
+{
+    /**
+    * Hide the title picker canvas and show the main UI
+    **/
+    public static bool LoadLowerThird(SceneData sceneData)
+    {
+        // Show the main canvas and the lower third panel inside it.
+        var mainCanvas = GameObject.Find("MainCanvas");
+        var lowerThirdPanel = mainCanvas.transform.Find("LowerThird");
+        lowerThirdPanel.gameObject.SetActive(true);
+
+        // Set the title to the lower third panel
+        Text segmentTitle = GameObject.Find("SegmentTitle").GetComponent<Text>();
+        segmentTitle.text = sceneData.segmentTitle;
+
+        // Hide the title picker canvas forever
+        GameObject.Find("TitlePickerCanvas").SetActive(false);
+        return true;
+    }
+}
---
+++ Assets/Scripts/TitleHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleHandler : MonoBehaviour
+{
+    public string title;
+    public GameObject sceneDataContainer;
+    private SceneData sceneData;
+
+    /**
+    * Initialization - store the sceneData.
+    **/
+    void Start()
+    {
+        sceneData = sceneDataContainer.transform.GetComponent<SceneData>();
+    }
+
+    /**
+    * Choose a title for your segment
+    **/
+    public void ChooseTitle()
+    {
+        sceneData.segmentTitle = title;
+
+        // A title has been chosen: disable all buttons
+        Button[] buttons = sceneDataContainer.transform.GetComponentsInChildren<Button>();
+        foreach (Button button in buttons)
+        {
+            button.interactable = false;
+        }
+        // Load the lower third panel
+        LowerThirdHandler.LoadLowerThird(sceneData);
+    }
+}
